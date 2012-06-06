281d480426f99f7b4922e5979e95168ba82b236b
Nick Markwell <nick@duckinator.net>
2012-06-06 16:05:19 -0400

    Initial commit

---
+++ song.rb
@@ -0,0 +1,32 @@
+$N="
+Never gonna"
+$a=" give"
+$y=" you up"
+$p="never gonna"+$a
+$P=$N+$a
+$g=$a+$y
+$l="#$N#$g#$N let you down#$N run around and desert you#$N make you cry#$N say goodbye#$N tell a lie and hurt you
+"
+$O="
+(Ooh"
+$W="
+We've known each other for so long
+Your heart's been aching but
+You're too shy to say it
+Inside we both know what's been going on
+We know the game and we're gonna play it"
+$h=" how I'm feeling"
+$I="
+I just wanna tell you#$h
+Gotta make you understand
+#$l"
+puts"We're no strangers to love
+You know the rules and so do I
+A full commitment's what I'm thinking of
+You wouldn't get this from any other guy#$I#$W
+And if you ask me#$h
+Don't tell me you're too blind to see
+"+$l*2+"#$O,#$g)"*2+"#$O)#$P, #$p
+(Give#$y)"*2+"
+#$W
+"+$I+$l*2

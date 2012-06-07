e6097391885880d713988c178bf74aa591461565
Nick Markwell <nick@duckinator.net>
2012-06-06 23:06:32 -0400

    677 bytes

--- two.rb
+++ two.rb
@@ -1,6 +1,6 @@
 def f s;s.gsub(/%?(\d+)/){[" you","\nYou","
 N11","
-2g6%2let0 down2run around and desert0%2make0 cry2say goodbye2tell a lie and hurt0"," know","7%10
+2g6%2let0 down2run around15desert0%2make0 cry2say goodbye2tell a lie15hurt0"," know","7%10
 Gotta make0 understand3","ive0 up","
 I just wanna tell0","
 (Ooh","8, g6)"," how I'm feeling","ever gonna ","11give","
@@ -9,9 +9,9 @@ N12, n12
 
 We've4n each other for so long1r heart's been aching but1're too shy to say it
 Inside we both4 what's been going on
-We4 the game and we're gonna play it
-"][$1.to_i]}end
-puts f f f f"We're no strangers to love1%4 the rules and so do I
+We4 the game15we're gonna play it
+"," and "][$1.to_i]}end
+puts f f f f"We're no strangers to love1%4 the rules15so do I
 A full commitment's what I'm thinking of1 wouldn't get this from any other guy5%14And if0 ask me10
 Don't tell me0're too blind to see3%3
 9%9%8)13(G6)8)13(G6)14%5%3%3"

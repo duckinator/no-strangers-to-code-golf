60d0b55074885cc8ca1f41e2b22e8048aa1c426a
Nick Markwell <nick@duckinator.net>
2012-06-06 22:29:52 -0400

    Hard-code it to 2 digits long. Shortens it even more.

--- two.rb
+++ two.rb
@@ -1,17 +1,17 @@
-def f s;s.gsub(/%(\d+)/){[" you","\nYou","
-N%11","
-%2g%6%2let%0 down%2run around and desert%0%2make%0 cry%2say goodbye%2tell a lie and hurt%0","
+def f s;s.gsub(/(\d\d)/){[" you","\nYou","
+N11","
+02g0602let00 down02run around and desert0002make00 cry02say goodbye02tell a lie and hurt00","
 
-We've known each other for so long%1r heart's been aching but%1're too shy to say it
+We've known each other for so long01r heart's been aching but01're too shy to say it
 Inside we both know what's been going on
 We know the game and we're gonna play it
 ","
-Gotta make%0 understand","ive%0 up","
-I just wanna tell%0","
-(Ooh","%8, g%6)"," how I'm feeling","ever gonna ","%11give","
-N%12, n%12
+Gotta make00 understand","ive00 up","
+I just wanna tell00","
+(Ooh","08, g06)"," how I'm feeling","ever gonna ","11give","
+N12, n12
 "][$1.to_i]}end
-puts f f f"We're no strangers to love%1 know the rules and so do I
-A full commitment's what I'm thinking of%1 wouldn't get this from any other guy%7%10%5%3%4And if%0 ask me%10
-Don't tell me%0're too blind to see%3%3
-%9%9%8)%13(G%6)%8)%13(G%6)%4%7%10%5%3%3%3"
+puts f f f"We're no strangers to love01 know the rules and so do I
+A full commitment's what I'm thinking of01 wouldn't get this from any other guy0710050304And if00 ask me10
+Don't tell me00're too blind to see0303
+090908)13(G06)08)13(G06)04071005030303"

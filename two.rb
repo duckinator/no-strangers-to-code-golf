a2e5744b5812070600bd5e733ad19d6e034720a1
Nick Markwell <nick@duckinator.net>
2012-06-06 21:14:12 -0400

    Added second implementation

---
+++ two.rb
@@ -0,0 +1,17 @@
+def f(s);s.gsub(/%(\d+)/){[" you","\nYou","
+N%11","
+%2g%6%2let%0 down%2run around and desert%0%2make%0 cry%2say goodbye%2tell a lie and hurt%0","
+
+We've known each other for so long%1r heart's been aching but%1're too shy to say it
+Inside we both know what's been going on
+We know the game and we're gonna play it
+","
+Gotta make%0 understand","ive%0 up","
+I just wanna tell%0","
+(Ooh","%8, g%6)"," how I'm feeling","ever gonna ","%11give","
+N%12, n%12
+"][$1.to_i]}end
+puts f f f "We're no strangers to love%1 know the rules and so do I
+A full commitment's what I'm thinking of%1 wouldn't get this from any other guy%7%10%5%3%4And if%0 ask me%10
+Don't tell me%0're too blind to see%3%3
+%9%9%8)%13(G%6)%8)%13(G%6)%4%7%10%5%3%3%3"

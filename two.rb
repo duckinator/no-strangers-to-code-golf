8ebe81817adc40dcc5aba6db8db24232c0daa63d
Nick Markwell <nick@duckinator.net>
2012-06-06 22:59:19 -0400

    two.rb is 688 bytes

--- two.rb
+++ two.rb
@@ -1,17 +1,17 @@
 def f s;s.gsub(/%?(\d+)/){[" you","\nYou","
 N11","
-2g6%2let0 down2run around and desert0%2make0 cry2say goodbye2tell a lie and hurt0","
-
-We've known each other for so long1r heart's been aching but1're too shy to say it
-Inside we both know what's been going on
-We know the game and we're gonna play it
-","
+2g6%2let0 down2run around and desert0%2make0 cry2say goodbye2tell a lie and hurt0"," know","
 Gotta make0 understand","ive0 up","
 I just wanna tell0","
 (Ooh","8, g6)"," how I'm feeling","ever gonna ","11give","
 N12, n12
+","
+
+We've4n each other for so long1r heart's been aching but1're too shy to say it
+Inside we both4 what's been going on
+We4 the game and we're gonna play it
 "][$1.to_i]}end
-puts f f f"We're no strangers to love1 know the rules and so do I
-A full commitment's what I'm thinking of1 wouldn't get this from any other guy7%10%5%3%4And if0 ask me10
+puts f f f"We're no strangers to love1%4 the rules and so do I
+A full commitment's what I'm thinking of1 wouldn't get this from any other guy7%10%5%3%14And if0 ask me10
 Don't tell me0're too blind to see3%3
-9%9%8)13(G6)8)13(G6)4%7%10%5%3%3%3"
+9%9%8)13(G6)8)13(G6)14%7%10%5%3%3%3"

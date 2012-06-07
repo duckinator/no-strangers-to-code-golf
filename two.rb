997ca840ac8f1b793c6f00bd85701af77c4aca70
Nick Markwell <nick@duckinator.net>
2012-06-06 22:44:37 -0400

    Switch back to %-based method, but make the % optional. Thanks @tsion!

--- two.rb
+++ two.rb
@@ -1,17 +1,17 @@
-def f s;s.gsub(/(\d\d)/){[" you","\nYou","
+def f s;s.gsub(/%?(\d+)/){[" you","\nYou","
 N11","
-02g0602let00 down02run around and desert0002make00 cry02say goodbye02tell a lie and hurt00","
+2g6%2let0 down2run around and desert0%2make0 cry2say goodbye2tell a lie and hurt0","
 
-We've known each other for so long01r heart's been aching but01're too shy to say it
+We've known each other for so long1r heart's been aching but1're too shy to say it
 Inside we both know what's been going on
 We know the game and we're gonna play it
 ","
-Gotta make00 understand","ive00 up","
-I just wanna tell00","
-(Ooh","08, g06)"," how I'm feeling","ever gonna ","11give","
+Gotta make0 understand","ive0 up","
+I just wanna tell0","
+(Ooh","8, g6)"," how I'm feeling","ever gonna ","11give","
 N12, n12
 "][$1.to_i]}end
-puts f f f"We're no strangers to love01 know the rules and so do I
-A full commitment's what I'm thinking of01 wouldn't get this from any other guy0710050304And if00 ask me10
-Don't tell me00're too blind to see0303
-090908)13(G06)08)13(G06)04071005030303"
+puts f f f"We're no strangers to love1 know the rules and so do I
+A full commitment's what I'm thinking of%01 wouldn't get this from any other guy7%10%5%3%4And if0 ask me10
+Don't tell me0're too blind to see3%3
+9%9%8)13(G6)8)13(G6)4%7%10%5%3%3%3"

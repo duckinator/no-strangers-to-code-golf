ab06b9b514017f3b6cba769133e1a9abb2fdcb0c
Nick Markwell <nick@duckinator.net>
2012-06-14 00:56:25 -0400

    \d# -> #\d; 697 -> 696 chars

--- two.rb
+++ two.rb
@@ -1,4 +1,4 @@
-def f s;s.gsub(/(\d+)#/,'\1%\1').gsub(/%?(\d+)/){[" you","\nYou","
+def f s;s.gsub(/#(\d+)/,'%\1%\1').gsub(/%?(\d+)/){[" you","\nYou","
 N11","
 2g6%2let0 down2run around15desert0%2make0 cry2say goodbye2tell a lie15hurt0"," know","7%10
 Gotta make0 understand3","ive0 up","
@@ -13,5 +13,5 @@ We4 the game15we're gonna play it
 "," and ","ing ","8)13(G6)"][$1.to_i]}end
 puts f f f f"We're no strangers to love1%4 the rules15so do I
 A full commitment's what I'm think16of1 wouldn't get this from any other guy5%14And if0 ask me10
-Don't tell me0're too blind to see3#
-9#%17#%14%5%3#"
+Don't tell me0're too blind to see#3
+#9#17%14%5#3"

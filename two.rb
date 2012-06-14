881e418d1b2820e68dc5eca7ffc27182a11723fe
Nick Markwell <nick@duckinator.net>
2012-06-14 01:19:32 -0400

    Revert "Still 696 chars, but more obscured."
    This reverts commit c2064950b1a2202bdd7dbffcfb13f4738ca318ab.

--- two.rb
+++ two.rb
@@ -7,11 +7,11 @@ I just wanna tell0","
 N12, n12
 ","
 
-We've4n each18for so long1r heart's been ach16but1're too shy to say it
+We've4n each other for so long1r heart's been ach16but1're too shy to say it
 Inside we both4 what's been go16on
 We4 the game15we're gonna play it
-"," and ","ing ","8)13(G6)"," other "][$1.to_i]}end
+"," and ","ing ","8)13(G6)"][$1.to_i]}end
 puts f f f f"We're no strangers to love1%4 the rules15so do I
-A full commitment's what I'm think16of1 wouldn't get this from any18guy5%14And if0 ask me10
+A full commitment's what I'm think16of1 wouldn't get this from any other guy5%14And if0 ask me10
 Don't tell me0're too blind to see#3
 #9#17%14%5#3"

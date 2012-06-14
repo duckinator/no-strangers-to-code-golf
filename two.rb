649c8c9707fac741cebaad946246472022a32c07
Nick Markwell <nick@duckinator.net>
2012-06-14 01:19:38 -0400

    Revert "697 chars! :D"
    This reverts commit f90d2164ce33392394ca49cf4c73d55f36f5f5fb.

--- two.rb
+++ two.rb
@@ -1,4 +1,4 @@
-def f s;s.gsub(/(\d+)#/,'\1%\1').gsub(/%?(\d+)/){[" you","\nYou","
+def f s;s.gsub(/%?(\d+)/){[" you","\nYou","
 N11","
 2g6%2let0 down2run around15desert0%2make0 cry2say goodbye2tell a lie15hurt0"," know","7%10
 Gotta make0 understand3","ive0 up","
@@ -10,8 +10,8 @@ N12, n12
 We've4n each other for so long1r heart's been ach16but1're too shy to say it
 Inside we both4 what's been go16on
 We4 the game15we're gonna play it
-"," and ","ing ","8)13(G6)"][$1.to_i]}end
+"," and ","ing "][$1.to_i]}end
 puts f f f f"We're no strangers to love1%4 the rules15so do I
 A full commitment's what I'm think16of1 wouldn't get this from any other guy5%14And if0 ask me10
-Don't tell me0're too blind to see3#
-9#%17#%14%5%3#"
+Don't tell me0're too blind to see3%3
+9%9%8)13(G6)8)13(G6)14%5%3%3"
